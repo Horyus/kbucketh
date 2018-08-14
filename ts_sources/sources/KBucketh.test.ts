5390a15b7ae42c2b587341afeae17708eaad4aa2
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2018-08-14 11:42:05 +0200

    Add constructor, create and getters
    KBKTH #3

--- ts_sources/sources/KBucketh.test.ts
+++ ts_sources/sources/KBucketh.test.ts
@@ -2,13 +2,77 @@ declare var describe;
 declare var test;
 declare var expect;
 
-import { KBucketh } from './KBucketh';
+import { create, KBucketh }       from './KBucketh';
+import { deserialize, serialize } from './Utility';
+import * as RandomString          from 'randomstring';
+import * as _                     from 'lodash';
 
-describe('Dummy Test Suite', () => {
+const my_id = '0x' + RandomString.generate({length: 40, charset: 'hex'});
+const other_id: string = deserialize(serialize('0x' + RandomString.generate({length: 40, charset: 'hex'})));
+const shared_registry = {
+    [other_id]: 123
+};
 
-    test('Testing KBucketh', () => {
-        const instance = new KBucketh();
-        expect(instance.test()).toBe('test');
+const ids = [];
+for (let generate_idx = 0; generate_idx < 10; ++generate_idx) {
+    ids.push(serialize('0x' + RandomString.generate({length: 40, charset: 'hex'})));
+}
+ids.push(serialize('0x0000000000000000000000000000000000000000'));
+ids.push(serialize('0xffffffffffffffffffffffffffffffffffffffff'));
+ids.push(serialize('0x0000000000000000000000000000000000000001'));
+ids.push(serialize('0x0000000000000000000000000000000000000100'));
+ids.push(serialize('0x0000000000000000000000000000010000000000'));
+ids.push(serialize('0x1010101010101010101010101010101010101010'));
+
+describe('KBucketh Test Suite', () => {
+
+    test('Creating manual KBucketh Instance with PeerID ' + my_id, () => {
+
+        const bucket = new KBucketh(my_id, 0, shared_registry, 15);
+        expect(_.isEqual(bucket.PeerID, serialize(my_id))).toBe(true);
+        expect(bucket.DeserializedPeerID).toBe(deserialize(serialize(my_id)));
+        expect(bucket.BucketID).toBe(0);
+        expect(bucket.size).toBe(15);
+        expect(bucket.limit).toBe(15);
+
+    });
+
+    test('Creating from create, without configuration', () => {
+        const bucket = create(my_id);
+        expect(bucket.BucketID).toBe(0);
+        expect(bucket.size).toBe(20);
+    });
+
+    test('Creating from create, with custom bit_distance', () => {
+        const bucket = create(my_id, {bit_distance: 2});
+        expect(bucket.BucketID).toBe(2);
+        expect(bucket.size).toBe(20);
+    });
+
+    test('Creating from create, with custom bucket size', () => {
+        const bucket = create(my_id, {bucket_size: 10});
+        expect(bucket.BucketID).toBe(0);
+        expect(bucket.size).toBe(10);
+    });
+
+    test('Creating from create, with custom bit_distance and bucket size', () => {
+        const bucket = create(my_id, {bit_distance: 2, bucket_size: 10});
+        expect(bucket.BucketID).toBe(2);
+        expect(bucket.size).toBe(10);
+    });
+
+    test('Getting bucketID of ' + other_id, () => {
+
+        const bucket = new KBucketh(my_id, 0, shared_registry, 15);
+        expect(bucket.bucketIDOf(other_id)).toBe(123);
+
+    });
+
+    test('Getting bucketID of ' + my_id, () => {
+
+        const bucket = new KBucketh(my_id, 0, shared_registry, 15);
+        expect(bucket.bucketIDOf(my_id)).toBe(-1);
+
     });
 
 });
--- ts_sources/sources/KBucketh.ts
+++ ts_sources/sources/KBucketh.ts
@@ -1,5 +1,109 @@
+import { deserialize, serialize } from './Utility';
+
+/**
+ * Interface used to store in which bucket every ID is stored.
+ * Shared between all buckets.
+ */
+export interface IPeerIdRegistry {
+    [key: string]: number;
+}
+
+/**
+ * Configuration interface for the {@link KBucketh} creator.
+ */
+export interface ICreateConfig {
+    bit_distance?: number;
+    bucket_size?: number;
+}
+
+/**
+ * Create the first KBucketh of the list. Use this function instead of the
+ * constructor.
+ *
+ * @param {any} peer_id ID of the local Peer.
+ * @param {ICreateConfig} config Configuration for the KBucketh.
+ */
+export function create(peer_id: any, config?: ICreateConfig): KBucketh {
+    const shared_peer_registry = {} as IPeerIdRegistry;
+    return (new KBucketh(
+        peer_id,
+        config ? (config.bit_distance || 0) : 0,
+        shared_peer_registry,
+        config ? (config.bucket_size || 20) : 20)
+    );
+}
+
+/**
+ * Main class. Self expanding double linked list. All nodes are aware of PeerID positions.
+ */
 export class KBucketh {
-    public test(): string {
-        return 'test';
+
+    private readonly _peer_id: Uint8Array;
+    private readonly _deserialized_peer_id: string;
+    private readonly _bit_distance: number;
+    private readonly _peer_id_registry: IPeerIdRegistry;
+    private readonly _bucket_size: number;
+
+    private _left_bucket: KBucketh = null;
+    private _right_bucket: KBucketh = null;
+
+    /**
+     * @param {any} peer_id ID of the local Peer.
+     * @param {number} bit_distance Bit distance of the current KBucketh.
+     * @param {IPeerIdRegistry} peer_id_registry Shared Peer Registry
+     * @param {number} bucket_size Size of the bucket.
+     */
+    public constructor(peer_id: any, bit_distance: number, peer_id_registry: IPeerIdRegistry, bucket_size: number) {
+        this._peer_id = serialize(peer_id);
+        this._deserialized_peer_id = deserialize(this._peer_id);
+        this._bit_distance = bit_distance;
+        this._peer_id_registry = peer_id_registry;
+        this._bucket_size = bucket_size;
     }
+
+    /**
+     * Return the ID of the Peer in its serialized form.
+     */
+    public get PeerID(): Uint8Array {
+        return this._peer_id;
+    }
+
+    /**
+     * Return the ID of the Peer in its deserialized form.
+     */
+    public get DeserializedPeerID(): string {
+        return this._deserialized_peer_id;
+    }
+
+    /**
+     * Return the ID of the KBucketh.
+     */
+    public get BucketID(): number {
+        return this._bit_distance;
+    }
+
+    /**
+     * Return the total size of the bucket.
+     */
+    public get limit(): number {
+        return this._bucket_size;
+    }
+
+    /**
+     * Return the current stored Peer count in this KBucketh.
+     */
+    public get size(): number {
+        return this._bucket_size;
+    }
+
+    /**
+     * Search in the registry the Bucket ID where the Peer ID is stored.
+     *
+     * @param {any} peer_id ID of the searched Peer.
+     */
+    public bucketIDOf(peer_id: any): number {
+        const deserialized = deserialize(serialize(peer_id));
+        return (this._peer_id_registry[deserialized] || -1);
+    }
+
 }
